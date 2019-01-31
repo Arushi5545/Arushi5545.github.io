39e8916215154fa393a0fcf5ed84205a7e4e6c61
Arushi Singh <arushisingh5545@gmail.com>
2019-01-31 01:00:41 -0500

    added buttons

--- homework3/country_viz.js
+++ homework3/country_viz.js
@@ -3,14 +3,19 @@
                 console.log("enter function changeFillRed() to update to Red")
                 console.log("enter function changeFillGreen() to update to Green")
                 console.log("enter function increaseRadius() to increase by 10")
-
+                //function changes fill to red
                 var changeFillRed=function(){
                     document.getElementById("circleGDP").style.fill = "red";
                 }
+                //function changes fill to green
                 var changeFillGreen=function(){
                     document.getElementById("circleGDP").style.fill = "green";
                 }
+                //function changes radius
                 var increaseRadius=function(){
                     document.getElementById("circleGDP").style.r = 10;
-
                 }
+                if (document.getElementById("buttonRed").onclick = true) {
+                    changeFillRed();
+                }
+               
