407fb68a72c77d9254cf1d80ded6e90082a17c9a
Arushi Singh <arushisingh5545@gmail.com>
2019-04-11 10:20:41 -0400

    updated

--- Studio_Religion/CityTriangulationFinal.js
+++ Studio_Religion/CityTriangulationFinal.js
@@ -1,114 +1,157 @@
 var pointsArray = [];
-
+var groupedReligion;
+var width = window.clientWidth;
+var height = window.clientHeight;
 //upload data set
-d3.csv("New_York_data.csv", function (data) {
-
-  var groupedReligion = d3.nest()
-    .key(function (row) {
-      return row.SUBTYPE;
-    })
-    .map(data);
-
-  console.log("grouped Religion", groupedReligion);
+//d3.csv("New_York_data.csv", function (data) {
+d3.csv("All_NewYork.csv", function (data) {
+// d3.csv("All_Places_of_Worship_final.csv", function (data) {
 
   //use mercator map projection to map X, Y points
   var projection = d3.geoMercator()
-    .scale(500000)
-    .center([-74.0060, 40.7128]);
+    .scale(100000)
+    .translate([300, 2000])
 
-  // //use geoAlbers map projection to map X, Y points
-  //   var projection = d3.geoAlbers()
-  //     .scale(10000)
-  //     .translate([-480, -300])
-
-
+  //to add projected X Y in the dataset
   data.forEach(function (d) {
-    tempArray = [projection([d.X, d.Y])[0], projection([d.X, d.Y])[1]];
-    console.log(projection([d.X, d.Y]));
-    pointsArray.push(tempArray);
+    d.XProjection = projection([d.X, d.Y])[0]
+    d.YProjection = projection([d.X, d.Y])[1]
+    // if(d.XProjection== 213.66519415499351){
+    //   console.log("outlier", d.NAME)
+    // }
   });
-  console.log("POINTS_ARRAY: ", pointsArray);
 
-  //select svg component
-  var svg = d3.select("svg"),
+  //to create nested groups first by city then by subtype
+  var GroupedCityReligion = d3.nest()
+    .key(function (d) { return d.CITY; })
+    .key(function (d) { return d.SUBTYPE; })
+    .entries(data);
+  console.log('grouped city religion', GroupedCityReligion)
 
-    //var svg = d3.select("svg").on("touchmove mousemove", moved),
-    width = +svg.attr("width"),
+  //run loop through each city, save the max min for that city, use that for displaying
+  GroupedCityReligion.forEach(function (dd) {
+    var xValues = [];
+    var yValues = [];
+    var sites = [];
+    //to select the city
+    console.log("key", dd.key);
+
+    if(dd.key === "NEW YORK"){
+      
+      dd.values.forEach(function (subtype_data) {
+      var religion = subtype_data.key;
+      subtype_data.values.forEach(function (val) {
+        if (subtype_data.length == 0) {
+          return false;
+        }
+        //  console.log("val",val);
+        sites.push([dd.XProjection, dd.YProjection]);
+        xValues.push(val.XProjection);
+        yValues.push(val.YProjection);
+      })
+    })
+    //  console.log(xValues);
+    //  console.log(yValues);
+    var maxX = d3.max(xValues);
+    var minX = d3.min(xValues);
+    var maxY = d3.min(yValues);
+    var minY = d3.max(yValues);
+    console.log("maxX", maxX);
+
+    //to center the projection
+    projection.center([(maxX - minX) / 2, (maxY - minY) / 2]);
+
+    var svg = d3.select("svg")
+
+    width = +svg.attr("width")
     height = +svg.attr("height");
 
-  var sites = pointsArray;
+    //calling vernoi function
+    var voronoi = d3.voronoi();
 
-  // var sites = d3.range(100)
-  //     .map(function(d) { return [Math.random() * width, Math.random() * height]; });
-  //     console.log("SITES ", sites);
+    //draws the paths for triangles
+    var triangle = svg.append("g")
+      .attr("class", "triangles")
+      .selectAll("path")
+      .data(voronoi.triangles(sites))
+      .enter().append("path")
+    // .call(redrawTriangle);
 
-  //calling vernoi function
-  var voronoi = d3.voronoi();
+    //draws the links for triangles  
+    var link = svg.append("g")
+      .attr("class", "links")
+      .selectAll("line")
+      .data(voronoi.links(sites))
+      .enter().append("line")
+      .call(redrawLink);
+    //console.log("link", link);
 
-  //draws the paths for triangles
-  var triangle = svg.append("g")
-    .attr("class", "triangles")
-    .selectAll("path")
-    .data(voronoi.triangles(sites))
-    .enter().append("path")
-  // .call(redrawTriangle);
+    //draws the circle points
+    var site = svg.append("g")
+      .attr("class", "sites")
+      .selectAll("circle")
+      .data(sites)
+      .enter().append("circle")
+      .text("a simple tooltip")
+      // .style("opacity", 5)
+      .attr("fill", function (d) {
+        if (religion === "CHRISTIAN") {
+          //console.log("I am CHRISTIAN");
+          return "#FCCA46"
+        }
+        else if (religion === "HINDU") { return "#FE7F2D" }
+        else if (religion === "BUDDHIST") { return "#0894A1" }
+        else if (religion === "MUSLIM") { return "#A1C181" }
+        else if (religion === "JUDAIC") { return "#233D4D" }
+        else if (religion === "OTHER") { return "#3F464C" }
+        else { return "black" }
+        ;
+      })
+      .attr("r", 2)
+      .call(redrawSite);
 
-  //draws the links for triangles  
-  var link = svg.append("g")
-    .attr("class", "links")
-    .selectAll("line")
-    .data(voronoi.links(sites))
-    .enter().append("line")
-    .call(redrawLink);
-  console.log("link", link);
+    //draws the triangulation
+    function redraw() {
+      var diagram = voronoi(sites);
+      triangle = triangle.data(diagram.triangles()), triangle.exit().remove();
+      triangle = triangle.enter().append("path").merge(triangle);
+      link = link.data(diagram.links()), link.exit().remove();
+      link = link.enter().append("line").merge(link).call(redrawLink);
+      site = site.data(sites).call(redrawSite);
+    }
 
-  //draws the circle points
-  var site = svg.append("g")
-    .attr("class", "sites")
-    .selectAll("circle")
-    .data(sites)
-    .enter().append("circle")
-    .style("opacity", 5)
-    .style("fill", "red")
-    .attr("r", 2.5)
-    .call(redrawSite);
 
-  //to redraw 
-  function moved() {
-    sites[0] = d3.mouse(this);
-    redraw();
+    function redrawLink(link) {
+      link
+        .classed("primary", function (d) { return d.source === sites[0] || d.target === sites[0]; })
+        .attr("x1", function (d) { return d.source[0]; })
+        .attr("y1", function (d) { return d.source[1]; })
+        .attr("x2", function (d) { return d.target[0]; })
+        .attr("y2", function (d) { return d.target[1]; })
+        .attr("stroke-width", 1)
+        .attr("opacity", 8)
+        // .attr("style", "mix-blend-mode : multiply")
+        .attr("stroke", function (d) {
+          if (religion === "CHRISTIAN") {
+            //console.log("I am CHRISTIAN");
+            return "#FCCA46"
+          }
+          else if (religion === "HINDU") { return "#FE7F2D" }
+          else if (religion === "BUDDHIST") { return "#0894A1" }
+          else if (religion === "MUSLIM") { return "#A1C181" }
+          else if (religion === "JUDAIC") { return "#233D4D" }
+          else if (religion === "OTHER") { return "#3F464C" }
+          else { return "black" }
+          ;
+        });
+
+    }
+
+    function redrawSite(site) {
+      site
+        .attr("cx", function (d) { return d[0]; })
+        .attr("cy", function (d) { return d[1]; });
+    }
+
   }
-
-  //draws the triangulation
-  function redraw() {
-    var diagram = voronoi(sites);
-    triangle = triangle.data(diagram.triangles()), triangle.exit().remove();
-    triangle = triangle.enter().append("path").merge(triangle).call(redrawTriangle);
-    link = link.data(diagram.links()), link.exit().remove();
-    link = link.enter().append("line").merge(link).call(redrawLink);
-    site = site.data(sites).call(redrawSite);
-  }
-
-  function redrawTriangle(triangle) {
-    triangle
-      .classed("primary", function (d) { return d[0] === sites[0] || d[1] === sites[0] || d[2] === sites[0]; })
-      .attr("d", function (d) { return "M" + d.join("L") + "Z"; });
-  }
-
-  function redrawLink(link) {
-    link
-      .classed("primary", function (d) { return d.source === sites[0] || d.target === sites[0]; })
-      .attr("x1", function (d) { return d.source[0]; })
-      .attr("y1", function (d) { return d.source[1]; })
-      .attr("x2", function (d) { return d.target[0]; })
-      .attr("y2", function (d) { return d.target[1]; });
-  }
-
-  function redrawSite(site) {
-    site
-      .attr("cx", function (d) { return d[0]; })
-      .attr("cy", function (d) { return d[1]; });
-  }
-
 });
-
--- Studio_Religion/CityTriangulationReligion.js
+++ Studio_Religion/CityTriangulationReligion.js
@@ -2,191 +2,286 @@ var pointsArray = [];
 var groupedReligion;
 var width = window.clientWidth;
 var height = window.clientHeight;
+var site;
 //upload data set
 //d3.csv("New_York_data.csv", function (data) {
 d3.csv("All_NewYork.csv", function (data) {
-// d3.csv("All_Places_of_Worship_final.csv", function (data) {
+  // d3.csv("dallas.csv", function (data) {
+  //  d3.csv("houston.csv", function (data) {
+      // d3.csv("Los_Angeles.csv", function (data) {
+    // d3.csv("chicago.csv", function (data) {
+      // d3.csv("philadelphia.csv", function (data) {
+    //  d3.csv("phoenix.csv", function (data) {
+    //  d3.csv("san antonio.csv", function (data) {
+    // d3.csv("san diego.csv", function (data) {
+    // d3.csv("dallas.csv", function (data) {
+      // d3.csv("san jose_1.csv", function (data) {
+    // d3.csv("All_Places_of_Worship_final.csv", function (data) {
 
-  //use mercator map projection to map X, Y points
-  var projection = d3.geoMercator()
-    .scale(100000)
-    .translate([300, 2000])
-    //new york
-    .center([-73.964835, 40.783281]);
-  //salt lake city
-  // .center([40, 60]);
-
-  //to add projected X Y in the dataset
-  data.forEach(function (d) {
-    d.XProjection = projection([d.X, d.Y])[0]
-    d.YProjection = projection([d.X, d.Y])[1]
-    // if(d.XProjection== 213.66519415499351){
-    //   console.log("outlier", d.NAME)
-    // }
-  });
-  
-  // //to create nested groups first by city then by subtype
-  // var GroupedCityReligion = d3.nest()
-  //   .key(function (d) { return d.CITY; })
-  //   .key(function (d) { return d.SUBTYPE; })
-  //   .entries(data);
-
-  //to group the data by religion
-  var groupedReligion = d3.nest()
-    .key(function (row) {
-      return row.SUBTYPE;
-    })
-    .entries(data);
-  //console.log(groupedReligion);
-  console.log("after grouping INSIDE", groupedReligion);
+    //use mercator map projection to map X, Y points
+    var projection = d3.geoMercator()
+      // default scale  
+      // .scale(50000)
+      .scale(50000)
+      //los Angeles
+        // .translate([39000, -7000])
+      // chicago
+      // .translate([12200, 1500])
+      //  philadelphia
+      //  .translate([1500, 2000])
+      // pheonix
+      //  .translate([33500, -6000])
+      // san antonio
+      // .translate([21700, -11000])
+      // san diago
+      //  .translate([37900, -8000])
+      // dallas
+      // .translate([20200, -8400])
+      // san jose
+      //  .translate([42000, -3500])
+      //houston
+      // .translate([19000, -11000])
+      //new york
+       .translate([100, 500])
+      //new york centre
+      .center([-73.964835, 40.783281]);
 
 
-  //To access all groups within Grouped Religion
-  for (var subtype in groupedReligion) {
-    var gdata = groupedReligion[subtype];
-    var religion = gdata.key;
-    // console.log(religion);
-    // console.log(subtype, gdata);
-    var sites = [];
-    //loop inside each object of grouped Religion
-    if (gdata.length == 0) {
-      return false;
-    }
-    gdata.values.forEach(function (dd) {
-      //console.log("length", dd);
-      // console.log("gdata[i]", gdata[i]);
-      //select svg component
-      sites.push([dd.XProjection, dd.YProjection])
+    //to add projected X Y in the dataset
+    data.forEach(function (d) {
+      d.XProjection = projection([d.X, d.Y])[0]
+      d.YProjection = projection([d.X, d.Y])[1]
+      // if(d.XProjection== 213.66519415499351){
+      //   console.log("outlier", d.NAME)
+      // }
     });
-    console.log(subtype, sites);
-    var svg = d3.select("svg")
 
-
-    width = +svg.attr("width")
-    height = +svg.attr("height");
-
-
-    //calling vernoi function
-    var voronoi = d3.voronoi();
-
-    //draws the paths for triangles
-    var triangle = svg.append("g")
-      .attr("class", "triangles")
-      .selectAll("path")
-      .data(voronoi.triangles(sites))
-      .enter().append("path")
-    // .call(redrawTriangle);
-
-    //draws the links for triangles  
-    var link = svg.append("g")
-      .attr("class", "links")
-      .selectAll("line")
-      .data(voronoi.links(sites))
-      .enter().append("line")
-      .call(redrawLink);
-    //console.log("link", link);
-
-    //draws the circle points
-    var site = svg.append("g")
-      .attr("class", "sites")
-      .selectAll("circle")
-      .data(sites)
-      .enter().append("circle")
-      // .style("opacity", 5)
-      .attr("fill", function (d) {
-        if (religion === "CHRISTIAN") {
-          //console.log("I am CHRISTIAN");
-          return "#FCCA46"
-        }
-        else if (religion === "HINDU") { return "#FE7F2D" }
-        else if (religion === "BUDDHIST") { return "#0894A1" }
-        else if (religion === "MUSLIM") { return "#A1C181" }
-        else if (religion === "JUDAIC") { return "#233D4D" }
-        else if (religion === "OTHER") { return "#3F464C" }
-        else { return "black" }
-        ;
+    //to group the data by religion
+    var groupedReligion = d3.nest()
+      .key(function (row) {
+        return row.SUBTYPE;
       })
-      .attr("r", 2)
-      .call(redrawSite);
+      .entries(data)
+      .sort(function (a, b) { return d3.descending(a.values, b.values) });
 
-    // //to add tooltips to  the points
-    // d3.selectAll(site)
-    //       .on("click", function(d) {
-    //           console.log(d);
-    //       })
-    //       .on("mousemove", function(d) {
-    //           var mouse = d3.mouse(document.body);
-    //           d3.select("#tooltip")
-    //               .style("display", "block")
-    //               .html("<h1>" + d.NAME + "</h1>")
-    //               .html("<h1>" + d.CITY + "</h1>")
-    //               .style("left", mouse[0] + "px")
-    //               .style("top", mouse[1] - 50 + "px");
-    //       })
-    //       .on("mouseout", function(d) {
-    //           d3.select("#tooltip")
-    //               .style("display", "none")
-    //       });
+    console.log(groupedReligion);
+    console.log("after grouping INSIDE", groupedReligion);
 
-
-    //draws the triangulation
-    function redraw() {
-      var diagram = voronoi(sites);
-      triangle = triangle.data(diagram.triangles()), triangle.exit().remove();
-      triangle = triangle.enter().append("path").merge(triangle);
-      link = link.data(diagram.links()), link.exit().remove();
-      link = link.enter().append("line").merge(link).call(redrawLink);
-      site = site.data(sites).call(redrawSite);
-    }
-
-    // function redrawTriangle(triangle) {
-    //   triangle
-    //     .classed("primary", function (d) { return d[0] === sites[0] || d[1] === sites[0] || d[2] === sites[0]; })
-    //     .attr("d", function (d) { return "M" + d.join("L") + "Z"; });
-    // }
-
-    function redrawLink(link) {
-      link
-        .classed("primary", function (d) { return d.source === sites[0] || d.target === sites[0]; })
-        .attr("x1", function (d) { return d.source[0]; })
-        .attr("y1", function (d) { return d.source[1]; })
-        .attr("x2", function (d) { return d.target[0]; })
-        .attr("y2", function (d) { return d.target[1]; })
-        .attr("stroke-width", 1)
-        .attr("opacity", 8)
-        // .attr("style", "mix-blend-mode : multiply")
-        .attr("stroke", function (d) {
-          if (religion === "CHRISTIAN") {
-            //console.log("I am CHRISTIAN");
-            return "#FCCA46"
-          }
-          else if (religion === "HINDU") { return "#FE7F2D" }
-          else if (religion === "BUDDHIST") { return "#0894A1" }
-          else if (religion === "MUSLIM") { return "#A1C181" }
-          else if (religion === "JUDAIC") { return "#233D4D" }
-          else if (religion === "OTHER") { return "#3F464C" }
-          else { return "black" }
-          ;
+    function drawThemAll(mod) {
+      //To access all groups within Grouped Religion
+      for (var subtype in groupedReligion) {
+        var gdata = groupedReligion[subtype];
+        var religion = gdata.key;
+        // console.log(religion);
+        // console.log(subtype, gdata);
+        var sites = [];
+        //loop inside each object of grouped Religion
+        if (gdata.length == 0) {
+          return false;
+        }
+        gdata.values.forEach(function (dd) {
+          //console.log("length", dd);
+          // console.log("gdata[i]", gdata[i]);
+          //select svg component
+          sites.push([dd.XProjection, dd.YProjection])
         });
 
+        console.log(subtype, sites);
+        var svg = d3.select("svg")
+
+
+        width = +svg.attr("width")
+        height = +svg.attr("height");
+
+
+        //calling vernoi function
+        var voronoi = d3.voronoi();
+
+        if (mod === "onlyLines") {
+          //draws FILLS
+          var triangle = svg.append("g")
+            .attr("class", "triangles")
+            .attr("opacity", 0.5)
+            .selectAll("path")
+            .data(voronoi.triangles(sites))
+            .enter().append("path")
+            .attr("fill", function (d) {
+              if (religion === "CHRISTIAN") {
+                //console.log("I am CHRISTIAN");
+                return "#EFD510"
+              }
+              else if (religion === "HINDU") { return "#E84822" }
+              else if (religion === "BUDDHIST") { return "#FF89BF" }
+              else if (religion === "MUSLIM") { return "#A1DE93" }
+              else if (religion === "JUDAIC") { return "#58A1D7" }
+              else if (religion === "OTHER") { return "#AC73FF" }
+              else { return "#E7E6E1" }
+              ;
+            })
+            .call(redrawTriangle, sites);
+
+          //draws OUTLINES  
+          var link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(voronoi.links(sites))
+            .enter().append("line")
+            .call(redrawLink, religion);
+          //console.log("link", link);
+        }
+
+        if (mod === "OnlyPoints") {
+          var site = svg.append("g")
+            .attr("class", "sites")
+            .selectAll("circle")
+            .data(sites)
+            .enter().append("circle")
+            // .on("mouseover", handleMouseOver)
+            // .on("mouseout", handleMouseOut)
+            .style("opacity", 1)
+            .attr("fill", function (d) {
+              if (religion === "CHRISTIAN") {
+                //console.log("I am CHRISTIAN");
+                return "#EFD510"
+              }
+              else if (religion === "HINDU") { return "#E84822" }
+              else if (religion === "BUDDHIST") { return "#FF89BF" }
+              else if (religion === "MUSLIM") { return "#A1DE93" }
+              else if (religion === "JUDAIC") { return "#58A1D7" }
+              else if (religion === "OTHER") { return "#AC73FF" }
+              else { return "#E7E6E1" }
+              ;
+            })
+            .attr("r", 2)
+            .call(redrawSite);
+        }
+
+        if (mod === "PointsAndLines") {
+          
+          //draws FILLS
+          var triangle = svg.append("g")
+            .attr("class", "triangles")
+            .attr("opacity", 0.5)
+            .selectAll("path")
+            .data(voronoi.triangles(sites))
+            .enter().append("path")
+            .attr("fill", function (d) {
+              if (religion === "CHRISTIAN") {
+                //console.log("I am CHRISTIAN");
+                return "#EFD510"
+              }
+              else if (religion === "HINDU") { return "#E84822" }
+              else if (religion === "BUDDHIST") { return "#FF89BF" }
+              else if (religion === "MUSLIM") { return "#A1DE93" }
+              else if (religion === "JUDAIC") { return "#58A1D7" }
+              else if (religion === "OTHER") { return "#AC73FF" }
+              else { return "#E7E6E1" }
+              ;
+            })
+            .call(redrawTriangle, sites);
+
+          //draws POINTS
+          var site = svg.append("g")
+            .attr("class", "sites")
+            .selectAll("circle")
+            .data(sites)
+            .enter().append("circle")
+            // .on("mouseover", handleMouseOver)
+            // .on("mouseout", handleMouseOut)
+            // .style("opacity", 1)
+            // .attr("style", "mix-blend-mode : multiply")
+            .attr("fill", function (d) {
+              if (religion === "CHRISTIAN") {
+                //console.log("I am CHRISTIAN");
+                return "#EFD510"
+              }
+              else if (religion === "HINDU") { return "#E84822" }
+              else if (religion === "BUDDHIST") { return "#FF89BF" }
+              else if (religion === "MUSLIM") { return "#A1DE93" }
+              else if (religion === "JUDAIC") { return "#58A1D7" }
+              else if (religion === "OTHER") { return "#AC73FF" }
+              else { return "#E7E6E1" }
+              ;
+            })
+            .attr("r", 3.5)
+            .call(redrawSite);
+
+          //draws OUTLINES  
+          var link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(voronoi.links(sites))
+            .enter().append("line")
+            .call(redrawLink, religion);
+          //console.log("link", link);
+
+
+
+
+
+        }
+      }
+
     }
 
-    function redrawSite(site) {
-      site
-        .attr("cx", function (d) { return d[0]; })
-        .attr("cy", function (d) { return d[1]; });
-    }
+    // drawThemAll("PointsAndLines");
+    drawThemAll("OnlyPoints");
+
+  });
 
 
+  function redrawSite(site) {
+    site
+      .attr("cx", function (d) { return d[0]; })
+      .attr("cy", function (d) { return d[1]; });
+  }
+  //console.log("new name")
 
-
+  function redrawLink(link, religion) {
+    link
+      .attr("x1", function (d) { return d.source[0]; })
+      .attr("y1", function (d) { return d.source[1]; })
+      .attr("x2", function (d) { return d.target[0]; })
+      .attr("y2", function (d) { return d.target[1]; })
+      .attr("stroke-width", 2.5)
+      //default opacity
+      .attr("opacity", 1)
+      // .attr("opacity", 1)
+      // .attr("style", "mix-blend-mode : hue")
+      .attr("stroke", function (d) {
+        if (religion === "CHRISTIAN") {
+          //console.log("I am CHRISTIAN");
+          return "#EFD510"
+        }
+        else if (religion === "HINDU") { return "#E84822" }
+        else if (religion === "BUDDHIST") { return "#FF89BF" }
+        else if (religion === "MUSLIM") { return "#A1DE93" }
+        else if (religion === "JUDAIC") { return "#58A1D7" }
+        else if (religion === "OTHER") { return "#AC73FF" }
+        else { return "#E7E6E1" }
+        //   return "#F9DC6A"
+        // }
+        // else if (religion === "HINDU") { return "#F07B3F" }
+        // else if (religion === "BUDDHIST") { return "#B83B5E" }
+        // else if (religion === "MUSLIM") { return "#17D567" }
+        // else if (religion === "JUDAIC") { return "#2E76FF" }
+        // else if (religion === "OTHER") { return "#6639A5" }
+        // else { return "#CECECE" }
+        ;
+      });
 
   }
 
-});
+  function redraw() {
+    var diagram = voronoi(sites);
+    triangle = triangle.data(diagram.triangles()), triangle.exit().remove();
+    triangle = triangle.enter().append("path").merge(triangle);
+    link = link.data(diagram.links()), link.exit().remove();
+    link = link.enter().append("line").merge(link).call(redrawLink);
+    site = site.data(sites).call(redrawSite);
+  }
 
-
-
-
-
-
+  function redrawTriangle(triangle, sites) {
+    triangle
+      .attr("d", function (d) { return "M" + d.join("L") + "Z"; });
+  }
